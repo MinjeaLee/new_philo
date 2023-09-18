822e304531c3580e4a016e3ea3c023ca71bb8981
LeeMinjea <gabe0529@naver.com>
2023-09-19 05:35:27 +0900

    time norm 맞춰서 수정

--- src/time.c
+++ src/time.c
@@ -6,22 +6,23 @@
 /*   By: mi <mi@student.42seoul.kr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 01:05:32 by mi                #+#    #+#             */
-/*   Updated: 2023/09/19 05:22:17 by mi               ###   ########.fr       */
+/*   Updated: 2023/09/19 05:35:14 by mi               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-long long get_time(void)
+long long	get_time(void)
 {
-	struct timeval tv;
+	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
-void ft_sleep(int time)
+
+void	ft_sleep(int time)
 {
-	long long curr;
+	long long	curr;
 
 	curr = get_time();
 	while (get_time() - curr < time)
