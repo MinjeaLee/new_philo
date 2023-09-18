8d6eba0653b9b0a1833a7e1a116ff72de2c05868
LeeMinjea <gabe0529@naver.com>
2023-09-19 05:32:32 +0900

    inti norm 맞춰서 수정

--- src/init.c
+++ src/init.c
@@ -6,13 +6,13 @@
 /*   By: mi <mi@student.42seoul.kr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 00:41:31 by mi                #+#    #+#             */
-/*   Updated: 2023/09/19 04:12:30 by mi               ###   ########.fr       */
+/*   Updated: 2023/09/19 05:31:49 by mi               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void init_config(t_resources *resources, int argc, char **argv)
+void	init_config(t_resources *resources, int argc, char **argv)
 {
 	resources->num_philosophers = ft_atoi(argv[1]);
 	resources->lifespan = ft_atoi(argv[2]);
@@ -24,10 +24,10 @@ void init_config(t_resources *resources, int argc, char **argv)
 		resources->min_eat_count = -1;
 }
 
-t_philo *init_philo(t_resources *resources)
+t_philo	*init_philo(t_resources *resources)
 {
-	t_philo *philo;
-	int i;
+	t_philo	*philo;
+	int		i;
 
 	i = 0;
 	philo = (t_philo *)malloc(sizeof(t_philo) * resources->num_philosophers);
@@ -40,17 +40,18 @@ t_philo *init_philo(t_resources *resources)
 		philo[i].finish_eat = 0;
 		philo[i].resources = resources;
 		philo[i].left_fork = &resources->fork[i];
-		philo[i].right_fork = &resources->fork[(i + 1) % resources->num_philosophers];
+		philo[i].right_fork = &resources->fork[(i + 1) % \
+										resources->num_philosophers];
 		i++;
 	}
 	return (philo);
 }
 
-t_resources *init_resources(int argc, char **argv)
+t_resources	*init_resources(int argc, char **argv)
 {
-	t_resources *resources;
-	int i;
-	
+	t_resources	*resources;
+	int			i;
+
 	resources = (t_resources *)malloc(sizeof(t_resources));
 	if (!resources)
 		return (NULL);
@@ -61,7 +62,7 @@ t_resources *init_resources(int argc, char **argv)
 	pthread_mutex_init(&resources->data_mutex, NULL);
 	pthread_mutex_init(&resources->print_mutex, NULL);
 	resources->fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * \
-													resources->num_philosophers);
+												resources->num_philosophers);
 	i = 0;
 	while (i < resources->num_philosophers)
 		pthread_mutex_init(&resources->fork[i++], NULL);
