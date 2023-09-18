c7aeced7f6a1e757bddc44cc0171efd16ffce3ea
LeeMinjea <gabe0529@naver.com>
2023-09-19 05:36:29 +0900

    status norm 맞춰서 수정

--- src/status.c
+++ src/status.c
@@ -6,22 +6,19 @@
 /*   By: mi <mi@student.42seoul.kr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 01:32:55 by mi                #+#    #+#             */
-/*   Updated: 2023/09/19 04:34:02 by mi               ###   ########.fr       */
+/*   Updated: 2023/09/19 05:36:12 by mi               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int philo_eat(t_philo *philo)
+int	philo_eat(t_philo *philo)
 {
 	take_forks(philo);
 	print_status(philo, "is eating");
 	ft_sleep(philo->resources->eat_duration);
 	drop_forks(philo);
-	// printf("1\n");
-	
 	pthread_mutex_lock(&philo->resources->data_mutex);
-	// printf("2\n");
 	philo->last_eat = get_time();
 	if (philo->resources->finish_program)
 	{
@@ -40,18 +37,18 @@ int philo_eat(t_philo *philo)
 	return (0);
 }
 
-void philo_sleep(t_philo *philo)
+void	philo_sleep(t_philo *philo)
 {
 	print_status(philo, "is sleeping");
 	ft_sleep(philo->resources->sleep_duration);
 }
 
-void philo_think(t_philo *philo)
+void	philo_think(t_philo *philo)
 {
 	print_status(philo, "is thinking");
 }
 
-void take_forks(t_philo *philo)
+void	take_forks(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->resources->data_mutex);
 	if (philo->resources->finish_program)
@@ -64,23 +61,9 @@ void take_forks(t_philo *philo)
 	print_status(philo, "has taken a fork");
 	pthread_mutex_lock(philo->right_fork);
 	print_status(philo, "has taken a fork");
-	// if (philo->id % 2 == 0)
-	// {
-	// 	pthread_mutex_lock(philo->left_fork);
-	// 	print_status(philo, "has taken a fork");
-	// 	pthread_mutex_lock(philo->right_fork);
-	// 	print_status(philo, "has taken a fork");
-	// }
-	// else
-	// {
-	// 	pthread_mutex_lock(philo->right_fork);
-	// 	print_status(philo, "has taken a fork");
-	// 	pthread_mutex_lock(philo->left_fork);
-	// 	print_status(philo, "has taken a fork");
-	// }
 }
 
-void drop_forks(t_philo *philo)
+void	drop_forks(t_philo *philo)
 {
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
