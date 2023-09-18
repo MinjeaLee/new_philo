f248aa7a2c6ddf3f86c0b355be0df89edcbeeeff
LeeMinjea <gabe0529@naver.com>
2023-09-19 05:34:26 +0900

    util norm 맞춰서 수정

--- src/utils.c
+++ src/utils.c
@@ -6,17 +6,17 @@
 /*   By: mi <mi@student.42seoul.kr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 00:52:23 by mi                #+#    #+#             */
-/*   Updated: 2023/09/19 04:24:14 by mi               ###   ########.fr       */
+/*   Updated: 2023/09/19 05:34:11 by mi               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int i;
-	int sign;
-	int result;
+	int	i;
+	int	sign;
+	int	result;
 
 	i = 0;
 	sign = 1;
@@ -37,13 +37,14 @@ int ft_atoi(const char *str)
 	return (result * sign);
 }
 
-void print_status(t_philo *philo, char *status)
+void	print_status(t_philo *philo, char *status)
 {
 	pthread_mutex_lock(&philo->resources->data_mutex);
 	if (philo->resources->finish_program != 1)
 	{	
 		pthread_mutex_lock(&philo->resources->print_mutex);
-		printf("%lld %d %s\n", get_time() - philo->resources->start_time, philo->id, status);
+		printf("%lld %d %s\n", get_time() - philo->resources->start_time, \
+														philo->id + 1, status);
 		pthread_mutex_unlock(&philo->resources->print_mutex);
 	}
 	pthread_mutex_unlock(&philo->resources->data_mutex);
